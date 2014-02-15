f221813a5b4b0b23b9e6517b7d555549fccd616a
Arnav <arnavsastry+github@gmail.com>
2014-02-15 17:51:04 -0600

    baby steps

--- js/context.js
+++ js/context.js
@@ -1,4 +1,14 @@
-chrome.contextMenus.create({id: 'tte', title: 'Track %s'}, function() {
+var clickHandler = function(info, tab) {
+    console.log(info);
+    console.log(tab);
+}
+
+chrome.contextMenus.create({
+    id: 'tte', 
+    title: 'Track This Element',
+    contexts: ['page', 'selection', 'link', 'image', 'video'],
+    onclick: clickHandler
+}, function() {
     if(chrome.extension.lastError) {
         console.log("Got error", chrome.extension.lastError);
     }
