f95685f5dedacaa7f54984b17a868fc396a43048
Arnav <arnavsastry+github@gmail.com>
2014-02-15 21:42:28 -0600

    THIS IS A BREAKING CHANGE

--- js/background.js
+++ js/background.js
@@ -1,3 +1,77 @@
+function getElementHash(el, tag)
+{
+    var hashKey = "H";
+    var hashContent = $.trim(el.html());
+    
+    if (hashContent === "")
+    {
+        hashContent = getElementAttributes(el);        
+        if (hashContent !== "")
+            hashKey = "A";
+        else
+            hashKey = "";
+    }
+    
+    if (hashKey !== "")
+        return(tag + ":" + hashKey + ":" + getHash(hashContent));
+    else
+        return "";
+}
+
+function getElementAttributes(el)
+{
+   var allAttribs = el[0].attributes;
+    attribs = "";
+    for(var i=0;i<allAttribs.length;i++) { 
+        attribs += allAttribs[i].nodeName + "-" + allAttribs[i].nodeValue + ";";
+    }
+    
+    return attribs;
+}
+
+function getElementByHash(hashValue)
+{
+    var el = null;
+    var frags = hashValue.split(":");
+    if (frags.length !== 3)
+        return null;
+    else
+    {
+        var tag = frags[0];
+        var hashKey = frags[1];
+        var hash = parseInt(frags[2]);
+
+        $("#main " + tag).each(function() {
+            if (hashKey == "H")
+            {
+                if ((getHash($(this).html()) - hash) == 0)
+                {
+                    el = $(this);
+                    return false;
+                }
+            }
+            else
+            {
+          
+                if ((getHash(getElementAttributes($(this))) - hash) == 0)
+                {
+                    el = $(this);
+                    return false;
+                }                    
+            }
+        });
+    }
+    return el;
+}
+
+function getHash(s) {
+        var hash = 0;
+        for (var i = 0; i < s.length; i++) {
+                hash = parseInt(s.charCodeAt(i)) + ( parseInt(hash << 5) - parseInt(hash) );
+        }
+        return hash;
+}
+
 var clickHandler = function(info, tab) {
     //link linkUrl defined
     //img mediaType: image
@@ -6,6 +80,13 @@ var clickHandler = function(info, tab) {
     var faviconUrl = tab.faviconUrl
       , url = tab.url
       , title = tab.title;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var id = tabs[0].id;
+        chrome.tabs.sendMessage(id, {greeting: 'hello'}, function(response) {
+            console.log(response);
+            console.log(faviconUrl, url, title);
+        });
+    });
 }
 
 chrome.contextMenus.create({
--- js/context.js
+++ js/context.js
@@ -1,15 +1,84 @@
-function find(el){
-    var path = [];
-    do {
-        path.unshift(el.nodeName + (el.className ? ' class="' + el.className + '"' : ''));
-    } while ((el.nodeName.toLowerCase() != 'html') && (el = el.parentNode));
+function getElementHash(el, tag)
+{
+    var hashKey = "H";
+    var hashContent = $.trim(el.html());
+    
+    if (hashContent === "")
+    {
+        hashContent = getElementAttributes(el);        
+        if (hashContent !== "")
+            hashKey = "A";
+        else
+            hashKey = "";
+    }
+    
+    if (hashKey !== "")
+        return(tag + ":" + hashKey + ":" + getHash(hashContent));
+    else
+        return "";
+}
 
-    console.log(path.join(" > "));
+function getElementAttributes(el)
+{
+   var allAttribs = el[0].attributes;
+    attribs = "";
+    for(var i=0;i<allAttribs.length;i++) { 
+        attribs += allAttribs[i].nodeName + "-" + allAttribs[i].nodeValue + ";";
+    }
+    
+    return attribs;
+}
+
+function getElementByHash(hashValue)
+{
+    var el = null;
+    var frags = hashValue.split(":");
+    if (frags.length !== 3)
+        return null;
+    else
+    {
+        var tag = frags[0];
+        var hashKey = frags[1];
+        var hash = parseInt(frags[2]);
+
+        $("#main " + tag).each(function() {
+            if (hashKey == "H")
+            {
+                if ((getHash($(this).html()) - hash) == 0)
+                {
+                    el = $(this);
+                    return false;
+                }
+            }
+            else
+            {
+          
+                if ((getHash(getElementAttributes($(this))) - hash) == 0)
+                {
+                    el = $(this);
+                    return false;
+                }                    
+            }
+        });
+    }
+    return el;
+}
+
+function getHash(s) {
+        var hash = 0;
+        for (var i = 0; i < s.length; i++) {
+                hash = parseInt(s.charCodeAt(i)) + ( parseInt(hash << 5) - parseInt(hash) );
+        }
+        return hash;
 }
 
 var target = null;
 document.addEventListener('mousedown', function(event) {
     target = event.target;
-    console.log(find(target));
 }, true);
 
+chrome.runtime.onMessage.addListener(function(req, sender, sendResponse) {
+    console.log(req, sender);
+    console.log(target.outerHTML);
+    sendResponse({html: target.outerHTML, hash: getElementHash(target, target.nodeName)});
+});
