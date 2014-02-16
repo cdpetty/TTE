b99b8a0335dd0ddd349da896095e9349b2d290a3
Arnav <arnavsastry+github@gmail.com>
2014-02-16 05:32:29 -0600

    oahisodghai

--- js/background.js
+++ js/background.js
@@ -1,77 +1,3 @@
-function getElementHash(el, tag)
-{
-    var hashKey = "H";
-    var hashContent = $.trim(el.html());
-
-    if (hashContent === "")
-    {
-        hashContent = getElementAttributes(el);        
-        if (hashContent !== "")
-            hashKey = "A";
-        else
-            hashKey = "";
-    }
-
-    if (hashKey !== "")
-        return(tag + ":" + hashKey + ":" + getHash(hashContent));
-    else
-        return "";
-}
-
-function getElementAttributes(el)
-{
-    var allAttribs = el[0].attributes;
-    attribs = "";
-    for(var i=0;i<allAttribs.length;i++) { 
-        attribs += allAttribs[i].nodeName + "-" + allAttribs[i].nodeValue + ";";
-    }
-
-    return attribs;
-}
-
-function getElementByHash(hashValue)
-{
-    var el = null;
-    var frags = hashValue.split(":");
-    if (frags.length !== 3)
-        return null;
-    else
-    {
-        var tag = frags[0];
-        var hashKey = frags[1];
-        var hash = parseInt(frags[2]);
-
-        $("#main " + tag).each(function() {
-            if (hashKey == "H")
-        {
-            if ((getHash($(this).html()) - hash) == 0)
-        {
-            el = $(this);
-            return false;
-        }
-        }
-            else
-        {
-
-            if ((getHash(getElementAttributes($(this))) - hash) == 0)
-        {
-            el = $(this);
-            return false;
-        }                    
-        }
-        });
-    }
-    return el;
-}
-
-function getHash(s) {
-    var hash = 0;
-    for (var i = 0; i < s.length; i++) {
-        hash = parseInt(s.charCodeAt(i)) + ( parseInt(hash << 5) - parseInt(hash) );
-    }
-    return hash;
-}
-
 //generate the next unique id (used to tell tracked instances that have the same url apart)
 var nextUniqueId = function(callback){
     chrome.storage.sync.get("uniqueid", function(found){
@@ -116,9 +42,9 @@ var clickHandler = function(info, tab) {
 
 chrome.contextMenus.create({
     id: 'tte', 
-title: 'Track This Element',
-contexts: ['page', 'selection', 'link', 'image', 'video'],
-onclick: clickHandler
+    title: 'Track This Element',
+    contexts: ['page', 'selection', 'link', 'image', 'video'],
+    onclick: clickHandler
 }, function() {
     if(chrome.extension.lastError) {
         console.log("Got error", chrome.extension.lastError);
--- js/context.js
+++ js/context.js
@@ -1,78 +1,15 @@
-function getElementHash(el, tag)
-{
-    var hashKey = "H";
-    var hashContent = $.trim(el.html());
-
-    if (hashContent === "")
-    {
-        hashContent = getElementAttributes(el);        
-        if (hashContent !== "")
-            hashKey = "A";
-        else
-            hashKey = "";
-    }
-
-    if (hashKey !== "")
-        return(tag + ":" + hashKey + ":" + getHash(hashContent));
-    else
-        return "";
-}
-
-function getElementAttributes(el)
-{
-    var allAttribs = el[0].attributes;
-    attribs = "";
-    for(var i=0;i<allAttribs.length;i++) { 
-        attribs += allAttribs[i].nodeName + "-" + allAttribs[i].nodeValue + ";";
-    }
-
-    return attribs;
-}
-
-function getElementByHash(hashValue)
-{
-    var el = null;
-    var frags = hashValue.split(":");
-    if (frags.length !== 3)
-        return null;
-    else
-    {
-        var tag = frags[0];
-        var hashKey = frags[1];
-        var hash = parseInt(frags[2]);
-
-        $("#main " + tag).each(function() {
-            if (hashKey == "H")
-        {
-            if ((getHash($(this).html()) - hash) == 0)
-        {
-            el = $(this);
-            return false;
-        }
-        }
-            else
-        {
-
-            if ((getHash(getElementAttributes($(this))) - hash) == 0)
-        {
-            el = $(this);
-            return false;
-        }                    
-        }
-        });
-    }
-    return el;
-}
-
-function getHash(s) {
-    var hash = 0;
-    for (var i = 0; i < s.length; i++) {
-        hash = parseInt(s.charCodeAt(i)) + ( parseInt(hash << 5) - parseInt(hash) );
-    }
-    return hash;
-}
-
 var target = null;
+function getElementPath(element)
+{
+    return $(element).parents().andSelf().map(function() {
+        var $this = $(this);
+        var tagName = this.nodeName;
+        if ($this.siblings(tagName).length > 0) {
+            tagName += "." + ($this.prevAll(tagName).length+1);
+        }
+        return tagName;
+    }).get().join("/").toUpperCase();
+}
 
 document.addEventListener('mousedown', function(event) {
     target = event.target;
@@ -81,5 +18,5 @@ document.addEventListener('mousedown', function(event) {
 chrome.runtime.onMessage.addListener(function(req, sender, sendResponse) {
     console.log(req, sender);
     console.log(target.outerHTML);
-    sendResponse({'html': target.outerHTML, 'hash': getElementHash($(target), target.nodeName)});
+    sendResponse({'html': target.outerHTML, 'hash': getElementPath(target)});
 });
